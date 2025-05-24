33ceb56e6d9de965645624b2467c0dcb2ae39199
danadavar <62427338+danadavar@users.noreply.github.com>
2025-05-24 17:22:24 +0200

    Update Jac_approx.m

--- Jac_approx.m
+++ Jac_approx.m
@@ -1,13 +1,44 @@
-function [A, H, nf] = Jac_approx(x, fvec, nf, tau, m, n, hfun, Ffun)
+function [A, H] = Jac_approx (x, fvec, tau, m, n, hfun, Ffun, lb, ub, x_min, f_min)
 
-H = [];
+H = zeros(n,1);
 I = eye(n);
 A = zeros(m,n);
 
-for j = 1:n
-  z = x+tau*I(:,j);
-  fvecz = Ffun(z);
-  nf = nf+1;
-  H = [H;hfun(fvecz)];
-  A(:,j) = (fvecz-fvec)/tau;
+if lb == -Inf && ub == Inf
+    for j = 1:n
+        z = x + tau*I(:,j);
+        fvecz = Ffun(z);
+        fz = hfun(fvecz);
+        H(j) = fz;
+        A(:, j) = (fvecz - fvec)/tau;
+
+        if fz < f_min
+            x_min = z;
+            f_min = fz;
+        end
+    end
+else
+    for j = 1:n
+        tau_F = min([ub-x(j) tau]);
+        tau_B = min([x(j)-lb tau]);
+
+        if tau_F >= tau_B
+            z = x + tau_F*I(:,j);
+            fvecz = Ffun(z);
+            fz = hfun(fvecz);
+            H(j) = fz;
+            A(:, j) = (fvecz - fvec)/tau_F;
+        else
+            z = x - tau_B*I(:,j);
+            fvecz = Ffun(z);
+            fz = hfun(fvecz);
+            H(j) = fz;
+            A(:, j) = (fvec - fvecz)/tau_B;
+        end
+
+        if fz < f_min
+            x_min = z;
+            f_min = fz;
+        end
+    end
 end
